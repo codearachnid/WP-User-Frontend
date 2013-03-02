da803b5983817b4f1435d4200f47070374466e12
Tareq Hasan <tareq1988@gmail.com>
2013-03-02 22:38:31 +0600

    unused menu removed

--- class/settings.php
+++ class/settings.php
@@ -33,10 +33,6 @@ class WPUF_Settings {
      */
     function admin_menu() {
         add_menu_page( __( 'WP User Frontend', 'wpuf' ), __( 'WP User Frontend', 'wpuf' ), 'delete_others_pages', 'wpuf-admin-opt', array($this, 'plugin_page'), null );
-        // add_submenu_page( 'wpuf-admin-opt', __( 'Custom Fields', 'wpuf' ), __( 'Custom Fields', 'wpuf' ), 'activate_plugins', 'wpuf_custom_fields', 'wpuf_custom_fields' );
-        //add_submenu_page( 'wpuf-admin-opt', 'Custom Taxonomies', 'Custom Taxonomies', 'activate_plugins', 'wpuf_custom_tax', 'wpuf_taxonomy_fields' );
-        // add_submenu_page( 'wpuf-admin-opt', __( 'Subscription', 'wpuf' ), __( 'Subscription', 'wpuf' ), 'activate_plugins', 'wpuf_subscription', 'wpuf_subscription_admin' );
-        // add_submenu_page( 'wpuf-admin-opt', __( 'Transaction', 'wpuf' ), __( 'Transaction', 'wpuf' ), 'activate_plugins', 'wpuf_transaction', 'wpuf_transaction' );
         add_submenu_page( 'wpuf-admin-opt', __( 'Settings', 'wpuf' ), __( 'Settings', 'wpuf' ), 'delete_others_pages', 'wpuf-settings', array($this, 'plugin_page') );
     }
 
